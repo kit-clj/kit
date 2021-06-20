c269f33d2847fe8363a8f861ccea73046c1f1342
dmitri.sotnikov@gmail.com <dmitri.sotnikov@gmail.com>
2021-06-20 13:12:03 -0400

    added clj-fmt

--- wake-generator/project.clj
+++ wake-generator/project.clj
@@ -2,10 +2,11 @@
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+            :url  "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.3"]
                  [hato "0.8.1"]
                  [selmer "1.12.40"]
                  [clj-jgit "1.0.1"]
+                 [clj-fmt "0.7.0"]
                  [org.clojure/tools.logging "1.1.0"]]
   :repl-options {:init-ns wake.generator.core})
