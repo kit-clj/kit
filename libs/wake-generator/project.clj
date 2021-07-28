22772eab7338553f7173e4fad8509469bc8fa5f8
dmitri.sotnikov@gmail.com <dmitri.sotnikov@gmail.com>
2021-07-28 16:41:17 -0400

    renamed wake-generator to wake/generator

--- libs/wake-generator/project.clj
+++ libs/wake-generator/project.clj
@@ -1,4 +1,4 @@
-(defproject wake-generator "0.1.0"
+(defproject wake/generator "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
