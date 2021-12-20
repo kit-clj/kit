56e0518adc15528d24c0ed0986437b21888df72c
dmitri.sotnikov@gmail.com <dmitri.sotnikov@gmail.com>
2021-12-20 15:46:38 -0500

    added log for starting/stopping the REPL server

--- libs/kit-repl/src/kit/edge/utils/repl.clj
+++ libs/kit-repl/src/kit/edge/utils/repl.clj
@@ -1,6 +1,7 @@
 (ns kit.edge.utils.repl
   (:require
     [clojure.core.server :as socket]
+    [clojure.tools.logging :as log]
     [integrant.core :as ig]))
 
 (defmethod ig/prep-key :repl/server
@@ -9,12 +10,18 @@
 
 (defmethod ig/init-key :repl/server
   [_ {:keys [port host name] :as config}]
-  (socket/start-server {:address host
-                        :port    port
-                        :name    name
-                        :accept  'clojure.core.server/repl})
+  (try
+    (socket/start-server {:address host
+                          :port    port
+                          :name    name
+                          :accept  'clojure.core.server/repl})
+    (log/info "REPL server started on host:" host "port:" port)
+    (catch Exception e
+      (log/error "failed to start the REPL server on host:" host "port:" port)
+      (throw e)))
   config)
 
 (defmethod ig/halt-key! :repl/server
   [_ config]
-  (socket/stop-server (:name config)))
+  (socket/stop-server (:name config))
+  (log/info "REPL server stopped"))
