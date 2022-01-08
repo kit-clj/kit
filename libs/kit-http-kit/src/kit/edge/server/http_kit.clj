b9d78b61f5247797f37dd4906f7d44316b028c4f
dmitri.sotnikov@gmail.com <dmitri.sotnikov@gmail.com>
2022-01-08 13:44:11 -0500

    added http-kit edge module

---
+++ libs/kit-http-kit/src/kit/edge/server/http_kit.clj
@@ -0,0 +1,50 @@
+(ns kit.edge.server.http-kit
+  (:require
+    [integrant.core :as ig]
+    [clojure.tools.logging :as log]
+    [org.httpkit.server :as http-kit]))
+
+(defn start [handler {:keys [host port] :as opts}]
+  (try
+    (log/info "starting HTTP server on port" port)
+    (http-kit/run-server
+     handler
+     (-> opts
+         (assoc  :legacy-return-value? false)
+         (dissoc :handler :init)))
+    (catch Throwable t
+      (log/error t (str "server failed to start on" host "port" port))
+      (throw t))))
+
+(defn stop [http-server timeout]
+  (let [result @(future (http-kit/server-stop! http-server {:timeout (or timeout 100)}))]
+     (log/info "HTTP server stopped")
+     result))
+
+(defmethod ig/prep-key :server/http
+  [_ config]
+  (merge {:port 3000
+          :host "0.0.0.0"}
+         config))
+
+(defmethod ig/init-key :server/http
+  [_ opts]
+  (let [handler (atom (delay (:handler opts)))]
+    {:handler handler
+     :server  (start (fn [req] (@@handler req)) (dissoc opts :handler))}))
+
+(defmethod ig/halt-key! :server/http
+  [{:keys [timeout]} {:keys [server]}]
+  (stop server timeout))
+
+(defmethod ig/suspend-key! :server/http
+  [_ {:keys [handler]}]
+  (reset! handler (promise)))
+
+(defmethod ig/resume-key :server/http
+  [k opts old-opts old-impl]
+  (if (= (dissoc opts :handler) (dissoc old-opts :handler))
+    (do (deliver @(:handler old-impl) (:handler opts))
+        old-impl)
+    (do (ig/halt-key! k old-impl)
+        (ig/init-key k opts))))
