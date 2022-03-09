1a27c3f66eeb112f32ec5281cf08e372be4ddab7
Marko Kocic <marko@euptera.com>
2022-03-09 17:40:51 +0100

    move versions.edn to lein-template

--- build.clj
+++ build.clj
@@ -8,7 +8,7 @@
    [deps-deploy.deps-deploy :as deploy]))
 
 (def libs-dir "libs")
-(def versions (read-string (slurp "versions.edn")))
+(def versions (read-string (slurp "./libs/lein-template/resources/versions.edn")))
 (def group-id "io.github.kit-clj")
 (def src ["src"])
 (def basis (b/create-basis {:project "deps.edn"}))
