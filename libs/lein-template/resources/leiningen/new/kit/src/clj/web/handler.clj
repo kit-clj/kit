fda08dca4c7119d5e5ca240ccab2a2d2ced3f67c
Eugen Stan <eugen.stan@netdava.com>
2022-09-11 00:10:07 +0300

    [#69] Added redirect-trailing-slash middleware

--- libs/lein-template/resources/leiningen/new/kit/src/clj/web/handler.clj
+++ libs/lein-template/resources/leiningen/new/kit/src/clj/web/handler.clj
@@ -13,14 +13,18 @@
       (ring/create-resource-handler {:path "/"})
       (when (some? api-path)
         (swagger-ui/create-swagger-ui-handler {:path api-path
-                                               :url  (str api-path "/swagger.json")}))
-      (ring/create-default-handler
-        {:not-found
-         (constantly {:status 404, :body "Page not found"})
-         :method-not-allowed
-         (constantly {:status 405, :body "Not allowed"})
-         :not-acceptable
-         (constantly {:status 406, :body "Not acceptable"})}))
+                                               :url  (str api-path "/swagger.json")})) 
+      (ring/routes
+        ;; Handle trailing slash in routes - add it + redirect to it
+        ;; https://github.com/metosin/reitit/blob/master/doc/ring/slash_handler.md
+        (ring/redirect-trailing-slash-handler)
+        (ring/create-default-handler
+         {:not-found
+          (constantly {:status 404, :body "Page not found"})
+          :method-not-allowed
+          (constantly {:status 405, :body "Not allowed"})
+          :not-acceptable
+          (constantly {:status 406, :body "Not acceptable"})})))
     {:middleware [(middleware/wrap-base opts)]}))
 
 (defmethod ig/init-key :router/routes
