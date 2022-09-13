5d121dbd34d4247b4a65be5801affa5be24d7f1c
Eugen Stan <eugen.stan@netdava.com>
2022-09-13 12:55:36 +0300

    [#71] Use single ring/routes call

--- libs/lein-template/resources/leiningen/new/kit/src/clj/web/handler.clj
+++ libs/lein-template/resources/leiningen/new/kit/src/clj/web/handler.clj
@@ -11,24 +11,23 @@
   (ring/ring-handler
     router
     (ring/routes
-      (ring/create-resource-handler {:path "/"})
-      (when (some? api-path)
-        (swagger-ui/create-swagger-ui-handler {:path api-path
-                                               :url  (str api-path "/swagger.json")})) 
-      (ring/routes
-        ;; Handle trailing slash in routes - add it + redirect to it
-        ;; https://github.com/metosin/reitit/blob/master/doc/ring/slash_handler.md
-        (ring/redirect-trailing-slash-handler)
-        (ring/create-default-handler
-         {:not-found
-          (constantly (-> {:status 404, :body "Page not found"}
-                          (http-response/content-type "text/html")))
-          :method-not-allowed
-          (constantly (-> {:status 405, :body "Not allowed"}
-                          (http-response/content-type "text/html")))
-          :not-acceptable
-          (constantly (-> {:status 406, :body "Not acceptable"}
-                          (http-response/content-type "text/html")))})))
+     ;; Handle trailing slash in routes - add it + redirect to it
+     ;; https://github.com/metosin/reitit/blob/master/doc/ring/slash_handler.md 
+     (ring/redirect-trailing-slash-handler)
+     (ring/create-resource-handler {:path "/"})
+     (when (some? api-path)
+       (swagger-ui/create-swagger-ui-handler {:path api-path
+                                              :url  (str api-path "/swagger.json")}))
+     (ring/create-default-handler
+      {:not-found
+       (constantly (-> {:status 404, :body "Page not found"}
+                       (http-response/content-type "text/html")))
+       :method-not-allowed
+       (constantly (-> {:status 405, :body "Not allowed"}
+                       (http-response/content-type "text/html")))
+       :not-acceptable
+       (constantly (-> {:status 406, :body "Not acceptable"}
+                       (http-response/content-type "text/html")))}))
     {:middleware [(middleware/wrap-base opts)]}))
 
 (defmethod ig/init-key :router/routes
