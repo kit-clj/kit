f0f972755cdc7fe4217fa353231f51c7d9e5fec9
Radford Smith <11401+rads@users.noreply.github.com>
2022-10-01 17:26:16 -0700

    Add failing test

--- test/kit/template_test.clj
+++ test/kit/template_test.clj
@@ -1,21 +1,21 @@
 (ns kit.template-test
   (:require [clojure.test :refer [deftest is]]
+            [clojure.string :as str]
             [clojure.java.shell :refer [sh]]))
 
-(deftest deps-new-test
-  (println
-    (sh "clojure"
-        "-Sdeps" "{:deps {io.github.kit-clj/kit {:local/root \".\"}}}"
-        "-T:deps-new"
-        ":template" "io.github.kit-clj/kit"
-        ":target-dir" "deps-new-app"
-        ":name" "yourname/app")))
-
-(deftest clj-new-test
-  (println
-    (sh "clojure"
-        "-Sdeps" "{:deps {io.github.kit-clj/kit {:local/root \".\"}}}"
-        "-T:new"
-        ":template" "io.github.kit-clj"
-        ":output" "clj-new-app"
-        ":name" "yourname/app")))
+(deftest deps-new-and-clj-new-parity-test
+  (sh "clojure"
+      "-Sdeps" "{:deps {io.github.kit-clj/kit {:local/root \".\"}}}"
+      "-T:new"
+      ":template" "io.github.kit-clj"
+      ":output" "clj-new-app"
+      ":name" "yourname/app")
+  (sh "clojure"
+      "-Sdeps" "{:deps {io.github.kit-clj/kit {:local/root \".\"}}}"
+      "-T:deps-new"
+      ":template" "io.github.kit-clj/kit"
+      ":target-dir" "deps-new-app"
+      ":name" "yourname/app")
+  (let [diff (str/split-lines (str/trim (:out (sh "diff" "--recursive" "--brief"
+                                                  "clj-new-app" "deps-new-app"))))]
+    (is (empty? diff))))
