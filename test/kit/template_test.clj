2f1bb610f6abcbb58d4fa654a4028ec272127527
Radford Smith <11401+rads@users.noreply.github.com>
2022-10-02 15:18:05 -0700

    Use temp dirs for template test

--- test/kit/template_test.clj
+++ test/kit/template_test.clj
@@ -1,30 +1,31 @@
 (ns kit.template-test
   (:require [clojure.test :refer [deftest is]]
             [clojure.string :as str]
-            [clojure.java.shell :refer [sh]]
-            [clojure.pprint :as pprint]
-            [clojure.edn :as edn]))
+            [babashka.fs :as fs]
+            [babashka.process :refer [sh]]))
 
 (deftest deps-new-and-clj-new-parity-test
-  (pprint/pprint
-    (sh "clojure"
-        "-Sdeps" "{:deps {io.github.kit-clj/kit {:local/root \".\"}}}"
-        "-T:new"
-        ":template" "io.github.kit-clj"
-        ":output" "clj-new-app"
-        ":name" "yourname/app"
-        ":force" "true"
-        ":args" "[+override-default-cookie-secret]"))
-  (pprint/pprint
-    (sh "clojure"
-        "-Sdeps" "{:deps {io.github.kit-clj/kit {:local/root \".\"}}}"
-        "-T:deps-new"
-        ":template" "io.github.kit-clj/kit"
-        ":target-dir" "deps-new-app"
-        ":name" "yourname/app"
-        ":default-cookie-secret" "test-secret"
-        ":overwrite" "delete"))
-  (let [{:keys [out]} (sh "diff" "--recursive" "--brief"
-                          "clj-new-app" "deps-new-app")
-        diff (some-> out str/trim not-empty str/split-lines)]
-    (is (empty? diff))))
+  (let [clj-new-path (doto (fs/create-temp-dir {:prefix "clj-new-app"})
+                       (fs/delete-on-exit))
+        deps-new-path (doto (fs/create-temp-dir {:prefix "deps-new-path"})
+                        (fs/delete-on-exit))]
+    (sh ["clojure"
+         "-Sdeps" "{:deps {io.github.kit-clj/kit {:local/root \".\"}}}"
+         "-T:new"
+         ":template" "io.github.kit-clj"
+         ":output" (pr-str (str clj-new-path))
+         ":name" "yourname/app"
+         ":force" "true"
+         ":args" "[+override-default-cookie-secret]"])
+    (sh ["clojure"
+         "-Sdeps" "{:deps {io.github.kit-clj/kit {:local/root \".\"}}}"
+         "-T:deps-new"
+         ":template" "io.github.kit-clj/kit"
+         ":target-dir" (pr-str (str deps-new-path))
+         ":name" "yourname/app"
+         ":default-cookie-secret" "test-secret"
+         ":overwrite" "delete"])
+    (let [{:keys [out]} (sh ["diff" "--recursive" "--brief"
+                             (str clj-new-path) (str deps-new-path)])
+          diff (some-> out str/trim not-empty str/split-lines)]
+      (is (empty? diff)))))
