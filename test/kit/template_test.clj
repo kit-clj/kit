f7babf0b424f838fbdf70ed045f0b91c7b3ea49b
Radford Smith <11401+rads@users.noreply.github.com>
2022-10-01 18:30:45 -0700

    Fix tests

--- test/kit/template_test.clj
+++ test/kit/template_test.clj
@@ -1,21 +1,30 @@
 (ns kit.template-test
   (:require [clojure.test :refer [deftest is]]
             [clojure.string :as str]
-            [clojure.java.shell :refer [sh]]))
+            [clojure.java.shell :refer [sh]]
+            [clojure.pprint :as pprint]
+            [clojure.edn :as edn]))
 
 (deftest deps-new-and-clj-new-parity-test
-  (sh "clojure"
-      "-Sdeps" "{:deps {io.github.kit-clj/kit {:local/root \".\"}}}"
-      "-T:new"
-      ":template" "io.github.kit-clj"
-      ":output" "clj-new-app"
-      ":name" "yourname/app")
-  (sh "clojure"
-      "-Sdeps" "{:deps {io.github.kit-clj/kit {:local/root \".\"}}}"
-      "-T:deps-new"
-      ":template" "io.github.kit-clj/kit"
-      ":target-dir" "deps-new-app"
-      ":name" "yourname/app")
-  (let [diff (str/split-lines (str/trim (:out (sh "diff" "--recursive" "--brief"
-                                                  "clj-new-app" "deps-new-app"))))]
+  (pprint/pprint
+    (sh "clojure"
+        "-Sdeps" "{:deps {io.github.kit-clj/kit {:local/root \".\"}}}"
+        "-T:new"
+        ":template" "io.github.kit-clj"
+        ":output" "clj-new-app"
+        ":name" "yourname/app"
+        ":force" "true"
+        ":args" "[+override-default-cookie-secret]"))
+  (pprint/pprint
+    (sh "clojure"
+        "-Sdeps" "{:deps {io.github.kit-clj/kit {:local/root \".\"}}}"
+        "-T:deps-new"
+        ":template" "io.github.kit-clj/kit"
+        ":target-dir" "deps-new-app"
+        ":name" "yourname/app"
+        ":default-cookie-secret" "test-secret"
+        ":overwrite" "delete"))
+  (let [{:keys [out]} (sh "diff" "--recursive" "--brief"
+                          "clj-new-app" "deps-new-app")
+        diff (some-> out str/trim not-empty str/split-lines)]
     (is (empty? diff))))
