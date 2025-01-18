dd189d8f0b2da0d47854a9c41c3901de03f87cae
Yogthos <yogthos@gmail.com>
2025-01-17 23:09:46 -0500

    bump up template version

--- libs/deps-template/resources/io/github/kit_clj/kit/src/clj/core.clj
+++ libs/deps-template/resources/io/github/kit_clj/kit/src/clj/core.clj
@@ -1,38 +1,37 @@
 (ns <<ns-name>>.core
   (:require
-    [clojure.tools.logging :as log]
-    [integrant.core :as ig]
-    [<<ns-name>>.config :as config]
-    [<<ns-name>>.env :refer [defaults]]
+   [clojure.tools.logging :as log]
+   [integrant.core :as ig]
+   [<<ns-name>>.config :as config]
+   [<<ns-name>>.env :refer [defaults]]
 
     ;; Edges<% if redis? %>
-    [kit.edge.cache.redis]<% endif %><% if xtdb? %>
-    [kit.edge.db.xtdb]<% endif %><% if hikari? %>
-    [kit.edge.db.sql.hikari]<% endif %><% if conman? %>
-    [kit.edge.db.sql.conman]<% endif %><% if migratus? %>
-    [kit.edge.db.sql.migratus]<% endif %><% if postgres? %>
-    [kit.edge.db.postgres]<% endif %><% if mysql? %>
-    [kit.edge.db.mysql]<% endif %><% if hato? %>
-    [kit.edge.http.hato]<% endif %><% if quartz? %>
-    [kit.edge.scheduling.quartz]<% endif %><% if selmer? %>
-    [kit.edge.templating.selmer]<% endif %><% if metrics? %>
-    [kit.edge.utils.metrics]<% endif %><% if repl? %>
-    [kit.edge.utils.repl]<% endif %><% if nrepl? %>
-    [kit.edge.utils.nrepl]<% endif %>
-    [kit.edge.server.undertow]
-    [<<ns-name>>.web.handler]
+   [kit.edge.cache.redis] <% endif %><% if xtdb? %>
+   [kit.edge.db.xtdb] <% endif %><% if hikari? %>
+   [kit.edge.db.sql.hikari] <% endif %><% if conman? %>
+   [kit.edge.db.sql.conman] <% endif %><% if migratus? %>
+   [kit.edge.db.sql.migratus] <% endif %><% if postgres? %>
+   [kit.edge.db.postgres] <% endif %><% if mysql? %>
+   [kit.edge.db.mysql] <% endif %><% if hato? %>
+   [kit.edge.http.hato] <% endif %><% if quartz? %>
+   [kit.edge.scheduling.quartz] <% endif %><% if selmer? %>
+   [kit.edge.templating.selmer] <% endif %><% if metrics? %>
+   [kit.edge.utils.metrics] <% endif %><% if repl? %>
+   [kit.edge.utils.repl] <% endif %><% if nrepl? %>
+   [kit.edge.utils.nrepl] <% endif %>
+   [kit.edge.server.undertow]
+   [<<ns-name>>.web.handler]
 
     ;; Routes
-    [<<ns-name>>.web.routes.api]
-    )
+   [<<ns-name>>.web.routes.api])
   (:gen-class))
 
 ;; log uncaught exceptions in threads
 (Thread/setDefaultUncaughtExceptionHandler
-  (fn [thread ex]
-      (log/error {:what :uncaught-exception
-                  :exception ex
-                  :where (str "Uncaught exception on" (.getName thread))})))
+ (fn [thread ex]
+   (log/error {:what :uncaught-exception
+               :exception ex
+               :where (str "Uncaught exception on" (.getName thread))})))
 
 (defonce system (atom nil))
 
