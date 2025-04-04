1d5935a8b8628b2fc729cdc426dab85f8e42c8d0
Luca Cervello <luca.cervello@gmail.com>
2025-04-04 17:04:27 +0200

    fix: resolve dependencies and install modules

--- libs/kit-generator/src/kit/generator/modules/dependencies.clj
+++ libs/kit-generator/src/kit/generator/modules/dependencies.clj
@@ -1,17 +1,11 @@
 (ns kit.generator.modules.dependencies)
 
 (defn resolve-dependencies
-  ([module feature-flag modules]
-   (resolve-dependencies module feature-flag modules '()))
-  ([module feature-flag modules dependencies]
-   (let [requires (get-in module [feature-flag :requires])]
-     (if (empty? requires)
-       dependencies
-       (into requires (mapcat #(resolve-dependencies (get modules %) modules dependencies) requires))))))
-
-(comment
-  (let [module  {:default {:requires [:html]}}
-        modules {:db   {:requires [:foo :bar]}
-                 :html {:requires [:db]}}]
-    (resolve-dependencies module :default modules))
-  )
+  ([module-config feature-flag]
+   (resolve-dependencies module-config feature-flag #{}))
+  ([module-config feature-flag reqs]
+   (let [requires (get-in module-config [feature-flag :requires] [])
+         feature-requires (get-in module-config [feature-flag :feature-requires])]
+     (if feature-requires
+       (into #{} (mapcat #(resolve-dependencies (dissoc module-config feature-flag) % requires) feature-requires))
+       (into reqs requires)))))
